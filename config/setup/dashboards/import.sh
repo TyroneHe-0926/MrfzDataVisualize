45d4ebbcc335dd91adf6558bf4fd0f4c5e5d2511
TyroneHe-0926 <tyrone99926@gmail.com>
2023-07-29 16:51:40 +0800

    finished setup

--- config/setup/dashboards/import.sh
+++ config/setup/dashboards/import.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-ES_USER=$1
-ES_PASSWORD=$2
+import()
+{
+    curl -u ${ES_USER}:${ELASTIC_PASSWORD} -X POST kibana:5601/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@$1
+}
 
 echo "Waiting for Kibana to launch on 5601"
 
+# for port 5601 to be avaliable
 while ! curl --output /dev/null --silent --head --fail kibana:5601; do 
     sleep 1 && echo -n .; 
 done;
 
-echo "Kibana launched"
+echo "Kibana launched, might take a while to setup the service"
 
-curl -u ES_USER:ES_PASSWORD -X POST localhost:5601/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@agents/akagents-dataview.ndjson
+# give kibana service 10s to be up
+sleep 10 && echo importing dashboards ...
 
-curl -u ES_USER:ES_PASSWORD -X POST localhost:5601/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@agents/akagents-spec-dashboard.ndjson
-
-curl -u ES_USER:ES_PASSWORD -X POST localhost:5601/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@agents/akagents-info-dashboard.ndjson
+import agents/akagents-dataview.ndjson
+import agents/akagents-spec-dashboard.ndjson
+import agents/akagents-info-dashboard.ndjson
+import news/aknews-dataview.ndjson
+import news/aknews-kibana-dashboard.ndjson
